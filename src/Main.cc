8bc50ad4ca91ff061e17b1b4fcad66735ee1f008
tezz-io <matblockgreninja@gmail.com>
2021-08-03 13:13:08 +0530

    Added tests for printFlags() and value()

--- src/Main.cc
+++ src/Main.cc
@@ -1,7 +1,11 @@
 #include "CommandLineParser.h"
+#include <cassert>
 
 int main( CommandLineArguments ) {
     CommandLineParser clp(ArgumentCount, ArgumentVector);
 
     clp.print();
+    clp.printFlags();
+    assert( clp.value("-l").compare("linker") == 0 );
+    assert( clp.value("-i").compare("include") == 0 );
 }
